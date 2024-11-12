8f9ee3561b2a4cf94fa8be65b9eda955a4678eea
Sanjai K <thisissanjai@gmail.com>
2024-11-12 16:28:58 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+setInterval(function() {
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    if (hour >= 6 && hour < 23) {
+        document.querySelector(".timer").innerHTML = `${23 - hour}:${60 - min}:${60 - sec}`;
+    } else {
+        document.querySelector(".timer").innerHTML = `Sleep Time`;
+    }
+
+},1000); 
